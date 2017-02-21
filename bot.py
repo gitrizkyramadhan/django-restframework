738a32a92f6a5f6b804f48d9be3a70e8ad5bb0d5
Agung Wahyu Kurniawan <spoor.badug@gmail.com>
2017-02-21 23:48:23 +0700

    - fixing bug image message

--- bot.py
+++ bot.py
@@ -66,12 +66,17 @@ class Bot(object):
         :param params:
         :return:
         """
+        user_ids = user_ids.split(",")
         if params is not None :
             thumbnail = params.get("thumbnail") if params.has_key("thumbnail") else None
-        user_ids = user_ids.split(",")
-        self.__send_multicast(user_ids, ImageSendMessage(
+            self.__send_multicast(user_ids, ImageSendMessage(
+                    original_content_url=img_url,
+                    preview_image_url=thumbnail if thumbnail is not None else img_url
+                ))
+        else :
+            self.__send_multicast(user_ids, ImageSendMessage(
                 original_content_url=img_url,
-                preview_image_url=thumbnail if thumbnail is not None else img_url
+                preview_image_url=img_url
             ))
 
     def send_video_message(self, user_ids, video_url, thumbnail_img_url):
