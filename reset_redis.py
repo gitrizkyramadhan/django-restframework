783a129508c9afdba42904de599457cc3649ed6d
Agung Wahyu Kurniawan <spoor.badug@gmail.com>
2017-03-03 21:16:19 +0700

    reset redis

--- reset_redis.py
+++ reset_redis.py
@@ -5,3 +5,5 @@ lineNlp = Nlp()
 
 lineNlp.redisconn.delete("profiling/U6fb98eb0f44be13523bbabd566e47dc4")
 lineNlp.redisconn.delete("profiling/Ud88f406a9eeb745ac5e62f4595e5b105")
+lineNlp.redisconn.delete("profiling/U90a846efb4bc03eec9e66cbf61fea960")
+lineNlp.redisconn.delete("profiling/U06ebb682542ad76886a4a202d9ac5094")
