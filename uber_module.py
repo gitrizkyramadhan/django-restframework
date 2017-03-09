372791dbb243d42dec815e486d547aabeae53318
agung.kurniawan <spoor.badug@gmail.com>
2017-03-09 22:38:40 +0700

    uber authorize

--- uber_module.py
+++ uber_module.py
@@ -4,11 +4,16 @@ from uber_rides.client import UberRidesClient
 from uber_rides.auth import AuthorizationCodeGrant
 
 
+SERVER_TOKEN = 'azt8FFGw80YQtGHEEglfibFTR9GlGUmojXdtGfHD'
+CLIENT_ID = 'EZx9ssXyrJFuWTqI_Unh_5guHIvAJyV9'
+CLIENT_SECRET = 'KZRRW1yVSJJVNrKLRVsS9AT89VvuLoUDHYhHKFtI'
+REDIRECT_URL = 'https://www.bangjoni.com/uber_token'
+
 class UberService():
 
     def __init__(self):
         print "Uber module loaded"
-        self.session = Session(server_token="23nuN6asAXNFCn1uaDkCPN1ZVfdnQvdi_pJbF0L3")
+        self.session = Session(server_token=SERVER_TOKEN)
         self.client = UberRidesClient(self.session)
 
     def get_products(self, origin, destination):
@@ -25,5 +30,14 @@ class UberService():
         estimate = response.json.get('prices')
         print json.dumps(response.json)
 
+    def authorize(self, origin, destination):
+        auth_flow = AuthorizationCodeGrant(CLIENT_ID,{'profile','request'},CLIENT_SECRET,REDIRECT_URL)
+        auth_url = auth_flow.get_authorization_url()
+        print auth_url
+        # session = auth_flow.get_session(auth_url)
+        # client = UberRidesClient(session, sandbox_mode=True)
+        # credentials = session.oauth2credential
+
 ub = UberService()
 ub.get_products({'lat':-6.2501281, 'lng':106.5996596}, {'lat':-6.2506642, 'lng':106.6244879})
+ub.authorize('','')
