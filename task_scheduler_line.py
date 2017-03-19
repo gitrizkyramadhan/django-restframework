98d12e615e6b4c006919d25778bd1cd8c6192363
Luky Prasetya <avoztrobe@gmail.com>
2017-03-20 02:37:32 +0700

    no message

--- task_scheduler_line.py
+++ task_scheduler_line.py
@@ -171,7 +171,7 @@ if __name__ == '__main__':
 
     scheduler = BlockingScheduler()
     # # scheduler.add_job(tick, 'interval', minutes=1)
-    scheduler.add_job(do_wheater_today, 'cron', hour=2, minute=10, args=["U90a846efb4bc03eec9e66cbf61fea960", "-6.946494", "107.613608"])
+    scheduler.add_job(do_wheater_today, trigger='cron', hour=2, minute=41, args=["U90a846efb4bc03eec9e66cbf61fea960", "-6.946494", "107.613608"])
     # #print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'$
     # # linebot.send_message("uba6616c505479974378dadbd15aaeb77", "TEST")
 
