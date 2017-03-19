b5e0a609ce5dcedcdcbf0890765c698a4303edea
Luky Prasetya <avoztrobe@gmail.com>
2017-03-19 15:53:51 +0700

    chsnge scheduler type

--- scheduler_s.py
+++ scheduler_s.py
@@ -4,17 +4,17 @@ from log_analytic import AnalyticLog
 from apscheduler.schedulers.background import BackgroundScheduler, BlockingScheduler
 
 def reminder_cuaca():
-    sched = BlockingScheduler()
+    sched = BackgroundScheduler()
     sched.start()
     al = AnalyticLog()
     # for data in al.get_reminder('cuaca'):
     sched.add_job(reminder_wheater_today, 'cron', hour='13', minute="33", args=["U90a846efb4bc03eec9e66cbf61fea960", "-6.946494", "107.613608"])
-    # try:
-    #     while True:
-    #         time.sleep(2)
-    # except (KeyboardInterrupt, SystemExit):
-    #     # Not strictly necessary if daemonic mode is enabled but should be done if possible
-    #     sched.shutdown()
+    # sched.add_job(print_somtehing, 'cron', hour='15', minute="39", args=None)
+    try:
+        while True:
+            time.sleep(1)
+    except (KeyboardInterrupt, SystemExit):
+        sched.shutdown()
 
 if __name__ == "__main__":
     
