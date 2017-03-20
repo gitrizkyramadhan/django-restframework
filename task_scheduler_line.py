7ba618093c145b053475d837b172e49b4ea7be8e
Luky Prasetya <avoztrobe@gmail.com>
2017-03-20 16:15:06 +0700

    no message

--- task_scheduler_line.py
+++ task_scheduler_line.py
@@ -121,22 +121,22 @@ def tick():
 def do_wheater_today(msisdn, longitude, latitude):
     weather_service = WeatherService()
     w_now = weather_service.get_wheather(longitude, latitude)
-    if w_now[0]['cuaca'].__contains__('HUJAN'):
-        columns = []
-        now_actions = []
+    # if w_now[0]['cuaca'].__contains__('HUJAN'):
+    columns = []
+    now_actions = []
 
-        column = {}
-        column['thumbnail_image_url'] = w_now['image']
-        column['title'] = 'Cuaca hari ini'
-        column['text'] = "Hari ini rata-rata %s" % (w_now['cuaca'])
-        if (len(column['text']) > 60):
-            column['text'] = column['text'][:57] + '...'
-        w_now.pop('image')
-        encoded_url = urllib.urlencode(w_now, doseq=True)
-        now_actions.append({'type': 'postback', 'label': 'Detailnya', 'data': encoded_url + "&evt=weather&day_type=today"})
-        column['actions'] = now_actions
-        columns.append(column)
-        linebot.send_composed_carousel(msisdn, "Cuaca", columns)
+    column = {}
+    column['thumbnail_image_url'] = w_now['image']
+    column['title'] = 'Cuaca hari ini'
+    column['text'] = "Hari ini rata-rata %s" % (w_now['cuaca'])
+    if (len(column['text']) > 60):
+        column['text'] = column['text'][:57] + '...'
+    w_now.pop('image')
+    encoded_url = urllib.urlencode(w_now, doseq=True)
+    now_actions.append({'type': 'postback', 'label': 'Detailnya', 'data': encoded_url + "&evt=weather&day_type=today"})
+    column['actions'] = now_actions
+    columns.append(column)
+    linebot.send_composed_carousel(msisdn, "Cuaca", columns)
 
 def reminder_cuaca():
     # sched = BackgroundScheduler()
