a6ca5328236b99765ab6fb15898026a6a908e90c
Luky Prasetya <avoztrobe@gmail.com>
2017-03-26 01:36:01 +0700

    no message

--- tasks.py
+++ tasks.py
@@ -3492,14 +3492,14 @@ def doworker(req):
                 address = urlparse.parse_qs(parsed.query)['address'][0]
                 linebot.send_location_message(msisdn, 'Alamat Restoran', address, lat, lng)
             elif postback_event == 'yes_reminder_weather':
-                city = urlparse.parse_qs(parsed.query)['city'][0]
-                insert_sql = "insert into reminder values(date_format(now(), '%Y%m%d%H%i%s'),'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s'")
-                % (msisdn, '1979-08-04 06:00:00', 'tiap', 'No', 'Everyday', 'Cuaca', city, 'line', '7')
+                city_reminder = urlparse.parse_qs(parsed.query)['city'][0]
+                insert_sql = "insert into reminder values(date_format(now(), '%Y%m%d%H%i%s'),'%s','%s','%s','%s','%s'," \
+                             "'%s','%s','%s','%s','%s'" % (msisdn, '1979-08-04 06:00:00', 'tiap', 'No', 'Everyday', 'Cuaca', city_reminder, 'line', '7')
                 insert(insert_sql)
             elif postback_event == 'no_reminder_weather':
-                city = urlparse.parse_qs(parsed.query)['city'][0]
-                insert_sql = "insert into reminder values(date_format(now(), '%Y%m%d%H%i%s'),'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s'")
-                % (msisdn, '1979-08-04 06:00:00', 'tiap', 'No', 'None', 'Cuaca', city, 'line', '7')
+                city_reminder = urlparse.parse_qs(parsed.query)['city'][0]
+                insert_sql = "insert into reminder values(date_format(now(), '%Y%m%d%H%i%s'),'%s','%s','%s','%s','%s'," \
+                             "'%s','%s','%s','%s','%s'" % (msisdn, '1979-08-04 06:00:00', 'tiap', 'No', 'None', 'Cuaca', city_reminder, 'line', '7')
                 insert(insert_sql)
 # ---------- DWP MODULE START ----------
 @app.task
